53de05b9cc7a1caa3e7183b1a0465ea0b501d064
zhangSong <1358995237@qq.com>
2024-08-21 18:26:32 +0800

    feat: 配置github action

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/Java-Interview/",
 
   lang: "zh-CN",
   title: "Java面经",
